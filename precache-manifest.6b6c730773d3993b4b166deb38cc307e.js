16c0c15a7585116d01806f2f9a33e5d502e7b4b7
HebertRyann <hebertryann40@gmail.com>
2021-06-28 16:55:44 -0300

    Updates

---
+++ precache-manifest.6b6c730773d3993b4b166deb38cc307e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8770bc11a2abf214d04ee5288b8a160c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "5523b47293e2a9a1a738",
+    "url": "/react-gh-pages/static/css/main.8acc30ba.chunk.css"
+  },
+  {
+    "revision": "14eaade879c9b27cc912",
+    "url": "/react-gh-pages/static/js/2.628fafea.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-gh-pages/static/js/2.628fafea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5523b47293e2a9a1a738",
+    "url": "/react-gh-pages/static/js/main.3ea47533.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/react-gh-pages/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "62e865e13ce4dd36023d6f8d6c53bb73",
+    "url": "/react-gh-pages/static/media/logo.62e865e1.svg"
+  }
+]);
